659f109f661e7764881a62f788a03444666893e7
admin <admin@tplwkpcente-a14>
2018-07-09 17:15:56 +0200

    pruebas en HomeController

--- src/main/java/com/agenda/spring/controller/HomeController.java
+++ src/main/java/com/agenda/spring/controller/HomeController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,6 +20,18 @@ import com.agenda.spring.services.IAgendaServices;
 @Controller
 public class HomeController {
 
+	
+	@RequestMapping("/Hola")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+	
+	
+	
+	
+	/*
+	
 	@Autowired
 	private IAgendaServices services;
 	
@@ -33,6 +47,8 @@ public class HomeController {
 		return model;
 			
 	}
+	
+	*/
 }
 
 
